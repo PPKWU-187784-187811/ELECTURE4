8fef3960f501f76a1db1261691473c680d228e2a
msut <marr994@o2.pl>
2016-12-03 17:41:52 +0100

    set simple route

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,14 @@
+import static spark.Spark.get;
+
 /**
  * Created by mariusz on 03.12.16.
  */
 public class Main {
 
     public static void main(String[] args){
-        System.out.println("Hello world!");
+        get("/", (req, res) -> {
+            return "Hello world!";
+        });
+
     }
 }
