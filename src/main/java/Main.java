c511f02cc3fb12c6d5e41a67b043d71c826d002a
AdamPiech <adampiech7@op.pl>
2016-12-06 08:33:04 +0100

    Added sceletons of HTTP methods.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,34 @@
-import static spark.Spark.get;
+import static spark.Spark.post;
 
 /**
  * Created by mariusz on 03.12.16.
  */
+
 public class Main {
 
     public static void main(String[] args){
-        get("/", (req, res) -> {
-            return "Hello world!";
+        post("/file/zip/", (req, res) -> {
+            return null;
         });
 
+        post("/file/unzip/", (req, res) -> {
+            return null;
+        });
+
+        post("/file/download", (req, res) -> {
+            return null;
+        });
+
+        post("/file/encrypt/", (req, res) -> {
+            return null;
+        });
+
+        post("/file/decrypt/", (req, res) -> {
+            return null;
+        });
+
+        post("/file/calculate", (req, res) -> {
+            return null;
+        });
     }
 }
