c58bc6f45fb237d63ec1863b5b795e1c24553eea
AdamPiech <adampiech7@op.pl>
2016-12-06 09:12:40 +0100

    Added impl download method & Fixes some bugs

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,8 @@
+import implementation.HTTPLibrary;
 import implementation.ZIPLibrary;
 import implementations.SimpleAesLibrary;
 import implementations.SimpleChecksumCalculator;
+import intefaces.IHTTPLibrary;
 import intefaces.IZIPLibrary;
 import interfaces.AesLibrary;
 import interfaces.ChecksumCalculator;
@@ -15,16 +17,17 @@ import static spark.Spark.post;
 public class Main {
 
     private static IZIPLibrary zipLibrary = new ZIPLibrary();
+    private static IHTTPLibrary httpLibrary = new HTTPLibrary();
     private static AesLibrary aesLibrary = new SimpleAesLibrary();
     private static ChecksumCalculator checksumCalculator = new SimpleChecksumCalculator();
 
     public static void main(String[] args) {
         post("/file/ziper/:source/:destination/:zipFileName/:password", (req, res) -> {
             try {
-                String source = ":source";
-                String destination = ":destination";
-                String zipFileName = ":zipFileName";
-                String password = ":password";
+                String source = req.params(":source");
+                String destination = req.params(":destination");
+                String zipFileName = req.params(":zipFileName");
+                String password = req.params(":password");
                 zipLibrary.compress(source, destination, zipFileName, password);
                 return "OK";
             } catch (Exception ex) {
@@ -35,24 +38,32 @@ public class Main {
 
         post("/file/unziper/:source/:destination/:password", (req, res) -> {
             try {
-                String source = ":source";
-                String destination = ":destination";
-                String password = ":password";
+                String source = req.params(":source");
+                String destination = req.params(":destination");
+                String password = req.params(":password");
                 zipLibrary.decompress(source, destination, password);
                 return "OK";
             } catch (Exception ex) {
                 res.status(HttpStatus.BAD_REQUEST_400);
                 return ex.getMessage();
             }
-        });
+        }, new JsonTransformer());
 
-        post("/file/downloader/", (req, res) -> {
-            return null;
-        });
+        post("/file/downloader/:url/:file", (req, res) -> {
+            try {
+                String url = req.params(":url");
+                String file = req.params(":file");
+                httpLibrary.downloadFile(url, file);
+                return "OK";
+            } catch (Exception ex) {
+                res.status(HttpStatus.BAD_REQUEST_400);
+                return ex.getMessage();
+            }
+        }, new JsonTransformer());
 
         post("/file/encryptor/:fileInputName/:fileOutputName/:key", (req, res) -> {
             try {
-                String fileInputNameParam = ":fileInputName";
+                String fileInputNameParam = req.params(":fileInputName");
                 String fileOutputNameParam = req.params(":fileOutputName");
                 String key = req.params(":key");
                 aesLibrary.encrypt(req.params(fileInputNameParam), fileOutputNameParam, key);
@@ -65,7 +76,7 @@ public class Main {
 
         post("/file/descriptor/:fileInputName/:fileOutputName/:key", (req, res) -> {
             try {
-                String fileInputNameParam = ":fileInputName";
+                String fileInputNameParam = req.params(":fileInputName");
                 String fileOutputNameParam = req.params(":fileOutputName");
                 String key = req.params(":key");
                 aesLibrary.decrypt(req.params(fileInputNameParam), fileOutputNameParam, key);
@@ -78,8 +89,8 @@ public class Main {
 
         post("/file/calculator/:fileInputName/:algorithm", (req, res) -> {
             try {
-                String fileInputNameParam = ":fileInputName";
-                String algorithmParam = ":algorithm";
+                String fileInputNameParam = req.params(":fileInputName");
+                String algorithmParam = req.params(":algorithm");
                 String sum = checksumCalculator.calculate(fileInputNameParam, algorithmParam);
                 return new ChecksumDto(sum);
             } catch (Exception ex) {
